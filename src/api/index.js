5a28c8fc06d33c65e556a1489b1c0ea4c1b649b0
wrj <1751353810@qq.com>
2025-01-20 23:51:26 +0800

    perf：https请求优化封装

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,8 @@
-import Axios from 'axios';
-import baseURL, { URLS } from './baseURL';
-
-const appkey = 'w123_1736868207199';
-
-const request = Axios.create({
-    baseURL,
-    params: {
-        appkey
-    }
-});
-
-const getSideList = () => request.get(URLS.getSide, { params: { appkey } });
-
-export default {
-    getSideList
+import {request} from "../utils/https.js";
+export function getSideApi(params) {
+  return request({
+    url: "/goods/getsidebar",
+    method: "get",
+    params
+  })
 }
